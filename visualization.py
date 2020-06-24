2ecff91f0e9c7705347395256402981e8cfed21e
ItsDrike <itsdrikeofficial@gmail.com>
2020-06-24 14:37:40 +0200

    Rework visualization - This is improved version for better readability.

--- visualization.py
+++ visualization.py
@@ -1,108 +1,124 @@
 import sys
 import pygame
-import typing as t
 import time
 from util import list_remap
+from collections import defaultdict
 
 
-SIZE = WIDTH, HEIGHT = 800, 350
-
-BLACK = 0, 0, 0
-WHITE = 255, 255, 255
-RED = 255, 0, 0
-GREEN = 0, 255, 0
-
-MINIMUM_LINE_LENGTH = 50
-SEPARATION = 8
-
-TICK_RATE = 10
+class Colors:
+    """This class is only used to store colors."""
+    WHITE = 255, 255, 255
+    BLACK = 0, 0, 0
+    RED = 255, 0, 0
+    GREEN = 0, 255, 0
+    BLUE = 0, 0, 255
+    ORANGE = 255, 165, 0
 
 
-def draw_lines(
-    screen: pygame.Surface,
-    main_color: t.Tuple[int, int, int],
-    checking_color: t.Tuple[int, int, int],
-    known_color: t.Tuple[int, int, int],
-    LIST: t.List[t.Union[int, float]],
-    checking: t.List[int],
-    known: t.List[int]
-) -> t.List[pygame.Rect]:
-    """Draw lines with every lines length corresponding to the value from list."""
-    LIST = list_remap(LIST, (0, HEIGHT - MINIMUM_LINE_LENGTH))
-    lines = []
-
-    for index, value in enumerate(LIST):
-        # Start with 20 units gap, and draw lines with SEPARATION units separation on X
-        x_pos = 20 + index * SEPARATION
-        # Get height on Y axis
-        y_pos = HEIGHT - value - MINIMUM_LINE_LENGTH
-        # Draw lines in `checking` and `known` with contrasting color
-        if index in checking:
-            color = checking_color
-        elif index in known:
-            color = known_color
-        else:
-            color = main_color
-
-        # Draw the lines and add them into `lines` list
-        line = pygame.draw.line(screen, color, (x_pos, HEIGHT), (x_pos, round(y_pos)))
-        lines.append(line)
-
-    return lines
+ARRAY = [12, 58, 21, 13, 18, 42, 35, 49, 10, 3, 50, 28, 55, 1, 8, 3, 24, 49]
+STATES = defaultdict(lambda: Colors.WHITE)
 
 
-def main_loop(screen: pygame.Surface, fps_clock: pygame.time.Clock, LIST: t.List[int]) -> None:
-    """Main loop which preforms bubble sort and visualizes the process"""
-    for m in range(1, len(LIST)):
-        for i in range(len(LIST) - m):
-            # Handle user quit
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    sys.exit()
+def bubble_sort(game: "Game") -> None:
+    """
+    Use bubble sort algorithm to sort any given list of
+    numbers or floats, this is being done in O(n^2).
+    """
+    for m in range(1, len(ARRAY)):
+        for i in range(len(ARRAY) - m):
+            # Pick 2 elements and compare them
+            # if first element is higher, swap them, else keep them
+            if ARRAY[i] > ARRAY[i + 1]:
+                # These lines are being swapped
+                STATES[i] = Colors.RED
+                STATES[i + 1] = Colors.RED
 
-            # Get 2 comparing elements on `i` and `i + 1`
-            element_1 = LIST[i]
-            element_2 = LIST[i + 1]
+                ARRAY[i], ARRAY[i + 1] = ARRAY[i + 1], ARRAY[i]
+            else:
+                # These lines won't be swapped
+                STATES[i] = Colors.BLUE
+                STATES[i + 1] = Colors.BLUE
 
-            # Reset the screen and redraw lines with `i` and `i + 1` as contrasting
-            screen.fill(BLACK)
-            draw_lines(screen, WHITE, RED, GREEN, LIST, [i, i + 1], list(range(len(LIST) - m + 1, len(LIST))))
+            # Update game window
+            game.update_screen()
 
-            # Update the screen and wait for tick
-            pygame.display.update()
-            fps_clock.tick(TICK_RATE)
-
-            # Check if elements needs to be swapped
-            if element_1 > element_2:
-                # Swap the elements in list
-                LIST[i] = element_2
-                LIST[i + 1] = element_1
-
-                # Reset the screen and redraw swapped lines (and highlight them)
-                screen.fill(BLACK)
-                draw_lines(screen, WHITE, RED, GREEN, LIST, [i, i + 1], list(range(len(LIST) - m + 1, len(LIST))))
-
-                # Update the screen and wait for tick
-                pygame.display.update()
-                fps_clock.tick(TICK_RATE)
+            # Reset the line colors
+            STATES[i] = Colors.WHITE
+            STATES[i + 1] = Colors.WHITE
+        # This line is already correctly sorted
+        STATES[len(ARRAY) - m] = Colors.GREEN
 
 
-def pygame_start(LIST: t.List[int]) -> None:
-    # Initialization
-    pygame.init()
-    screen = pygame.display.set_mode(SIZE)
-    fps_clock = pygame.time.Clock()
+class Game:
+    # How far are individual lines from each other
+    SEPARATION = 8
+    # How fast should the tick rate be
+    TICK_RATE = 10
+    # Set window parameters
+    SIZE = WIDTH, HEIGHT = 400, 350
 
-    # Don't start straight away
-    time.sleep(3)
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode(Game.SIZE)
+        self.fps_clock = pygame.time.Clock()
 
-    main_loop(screen, fps_clock, LIST)
+    def handle_user_quit(self) -> None:
+        """If user quits, exit the game and stop program."""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
 
-    # Don't close right away
-    time.sleep(3)
+    def redraw_screen(self) -> None:
+        """
+        Redraw all lines on the screen.
+
+        This does not update the screen, it only redraws it.
+        """
+        # Reset screen to black
+        self.screen.fill(Colors.BLACK)
+
+        # Map values from array onto pygame window height
+        arr = list_remap(ARRAY, (0, Game.HEIGHT))
+
+        # Draw individual lines
+        for index, value in enumerate(arr):
+            # Round the value before working with it
+            # This is necessary because pygame doesn't accept floats
+            value = round(value)
+
+            # Start with 10 units gap, draw lines with given separation between them
+            x_pos = 10 + index * Game.SEPARATION
+            # Subtract the value from height, pygame is inverted on Y axis
+            y_pos = Game.HEIGHT - value
+
+            pos1 = (x_pos, Game.HEIGHT)
+            pos2 = (x_pos, y_pos)
+
+            color = STATES[index]
+
+            pygame.draw.line(self.screen, color, pos1, pos2)
+
+    def update_screen(self, tick: bool = True) -> None:
+        """
+        Update the screen accordingly to `redraw_screen`
+        also check for user quit and tick (until specified otherwise).
+        """
+        self.handle_user_quit()
+        self.redraw_screen()
+
+        # Update the display and tick when needed
+        pygame.display.update()
+        if tick:
+            self.fps_clock.tick(Game.TICK_RATE)
 
 
-LIST = [12, 58, 21, 13, 18, 42, 35, 49, 10, 3, 50, 28, 55, 1, 8, 3, 24, 49]
+game = Game()
+# Starting timeout
+time.sleep(3)
 
-pygame_start(LIST)
+bubble_sort(game)
+game.update_screen()
+
+# Don't stop straight away
+time.sleep(3)
